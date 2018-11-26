9e80e6243b498170c7ac32162ac761fc08474400
nickgood <n.good@colostate.edu>
2018-11-26 15:10:05 -0700

    deal with NA moy in error handling

--- R/date_to_season.R
+++ R/date_to_season.R
@@ -8,11 +8,13 @@ date_to_season <- function(date){
   moy <- tryCatch(lubridate::month(date),
            error = function(e){print(paste("non-date argument", date));NaN})
 # calculate season
- if(moy > 2 & moy < 6){season <- "spring"}
- else if(moy > 5 & moy < 9){season <- "summer"}
- else if(moy > 8 & moy < 12){season <- "fall"}
- else if(moy > 11 | moy < 3){season <- "winter"}
- else(NA)
+ if(is.na(moy)){NA}
+ else{
+  if(moy > 2 & moy < 6){season <- "spring"}
+  if(moy > 5 & moy < 9){season <- "summer"}
+  if(moy > 8 & moy < 12){season <- "fall"}
+  if(moy > 11 | moy < 3){season <- "winter"}
+ }
 # output
  return(season)
 }
