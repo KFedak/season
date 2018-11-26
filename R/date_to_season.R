fcc708b15de78de4535c88c4fe10a7916e7c9db8
nickgood <n.good@colostate.edu>
2018-11-26 14:54:49 -0700

    function to calculate the season from the month of the year

--- R/date_to_season.R
+++ R/date_to_season.R
@@ -1,7 +1,20 @@
 # date to season!
 #
 # this function converts a date to a season
-
-date_to_season <- function(date) {
-  print("season")
+date_to_season <- function(date){
+# empty output factor
+  season <- factor(levels = c("spring", "summer", "fall", "winter"))
+# check if a month can be extracted
+  moy <- tryCatch(lubridate::month(date),
+           error = function(e){print(paste("non-date argument", date));NaN})
+# calculate season
+ if(moy > 2 & moy < 6){season <- "spring"}
+ else if(moy > 5 & moy < 9){season <- "summer"}
+ else if(moy > 8 & moy < 12){season <- "fall"}
+ else if(moy > 11 | moy < 3){season <- "winter"}
+ else(NA)
+# output
+ return(season)
 }
+
+
